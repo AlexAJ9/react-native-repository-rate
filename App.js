d2ae6ccd97023299ca4bf43ecf83211c384ae59f
AlexAj9 <a4319@mail.bg>
2021-12-07 23:11:56 +0200

    10.3

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Main from "./src/components/Main";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Ye</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Main />;
 }
 
 const styles = StyleSheet.create({
