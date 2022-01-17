7a8ea299f6977658eec53fa1f4792766b12bb372
AlexAj9 <a4319@mail.bg>
2022-01-17 23:06:02 +0200

    scroll view

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,23 +1,25 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import Constants from "expo-constants";
 import AppBarTab from "./AppBarTab";
 import theme from "../theme";
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
-    flexDirection: "row",
     backgroundColor: theme.colors.tabBackground,
     paddingTop: Constants.statusBarHeight,
+    display: "flex",
+    flexDirection: "row",
   },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab text="Repositories" />
-      <AppBarTab url="sign-in" text="Sign in" />
+      <ScrollView horizontal>
+        <AppBarTab text="Repositories" />
+        <AppBarTab url="sign-in" text="Sign in" />
+      </ScrollView>
     </View>
   );
 };
